6b84a5fd23f557715e0e38ae1f176e5cc70c267d
1 <1@DESKTOP-F8EP83E>
2020-07-02 20:49:18 +0800

    DDSZ

--- IOT_ErpManageSystem.BLL/RBAC_Allot/RBAC_Allot.cs
+++ IOT_ErpManageSystem.BLL/RBAC_Allot/RBAC_Allot.cs
@@ -3,6 +3,7 @@ using IOT_ErpManageSystem.DAL.DBHelper;
 using IOT_ErpManageSystem.Models;
 using IOT_ErpManageSystem.Models.Models;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
